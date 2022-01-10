27c63103e7fd131093614ce57defbe27e5c292da
Shrihith A <70382218+Shrihith@users.noreply.github.com>
2022-01-10 17:21:17 +0530

    up

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -24,20 +24,19 @@ let render_projects = (slug) => {
             categories: ['Workshops']
         },
         {
-            image: 'assets/images/3-Quantum Mechanics,Molecular Mechanics and Metadynamics Workshop ',
+            image: 'assets/images/3-Quantum Mechanics,Molecular Mechanics and Metadynamics Workshop.jpeg ',
             title: 'Quantum Mechanics,Molecular Mechanics and Metadynamics Workshop',
             technologies: ['Molecular Dynamics', 'GROMACS'],
             description: "2 days workshop on Molecular Dynamics and simulations.",
             categories: ['Workshops']
         }, {
-            image: 'assets/images/mpw.jpg',
-            link: 'https://github.com/abhn/mpw',
-            title: 'Master Password',
-            demo: 'https://www.nagekar.com/mpw',
-            technologies: ['Semantic UI', 'CSS3'],
-            description: "Master Password is an ingenious password solution that makes your passwords truly impossible to lose.",
-            categories: ['featured', 'security']
-        }, {
+            image: 'assets/images/4-R for Biologist__Certificate1.png',
+            title: 'R for Biologist',
+            technologies: ['R', 'Visualization', 'Research'],
+            description: "R programming for Biologist learn Research visualization and Statistical analysis.",
+            categories: ['Workshops']
+        },
+        {
             image: 'assets/images/social-share-count.jpeg',
             link: 'https://github.com/abhn/Social-Share-Counts',
             title: 'Social Share Count',
@@ -45,7 +44,8 @@ let render_projects = (slug) => {
             technologies: ['Python'],
             description: "Ever wondered how many times a URL has been shared on popular social networks?",
             categories: ['native']
-        }, {
+        },
+        {
             image: 'assets/images/data-destroyer.png',
             link: 'https://github.com/abhn/data-destroyer-gui',
             title: 'Data Destroyer',
@@ -53,7 +53,8 @@ let render_projects = (slug) => {
             technologies: ['C++', 'Qt'],
             description: "Native GUI wrapper for GNU coreutils tool 'dd'",
             categories: ['native']
-        }, {
+        },
+        {
             image: 'assets/images/raspberry-pi-monitor.png',
             link: 'https://github.com/abhn/RPi-Status-Monitor',
             title: 'Raspberry Pi Monitor',
@@ -61,7 +62,8 @@ let render_projects = (slug) => {
             technologies: ['python', 'flask'],
             description: "Web based status monitor/smart mirror, displays system stats, weather and more.",
             categories: ['webdev', 'diy']
-        }, {
+        },
+        {
             image: 'assets/images/s3scan.png',
             link: 'https://github.com/abhn/S3Scan',
             title: 'S3Scan',
@@ -69,7 +71,8 @@ let render_projects = (slug) => {
             technologies: ['python'],
             description: "Automate crawling of a website and find publicly open S3 buckets for takeover.",
             categories: ['native', 'security']
-        }, {
+        },
+        {
             image: 'assets/images/elementary.png',
             link: 'https://github.com/abhn/Elementary',
             title: 'Elementary',
@@ -77,7 +80,8 @@ let render_projects = (slug) => {
             technologies: ['Jekyll', 'CSS3'],
             description: "Elementary is a zero Javascript and minimal CSS ultra lightweight Jekyll theme for those of you who love simplicity.",
             categories: ['webdev']
-        }, {
+        },
+        {
             image: 'assets/images/soot-spirits.png',
             link: 'https://github.com/abhn/Soot-Spirits',
             title: 'Soot Spirits',
@@ -85,7 +89,8 @@ let render_projects = (slug) => {
             technologies: ['Jekyll', 'CSS3'],
             description: "A simple responsive two column Jekyll theme. Great for personal blog and basic portfolio website.",
             categories: ['webdev']
-        }, {
+        },
+        {
             image: 'assets/images/python-chat.png',
             link: 'https://www.nagekar.com/2014/12/lan-group-messenger-in-python.html',
             title: 'Terminal Group Chat',
@@ -93,7 +98,8 @@ let render_projects = (slug) => {
             technologies: ['Python', 'Sockets'],
             description: "Simple terminal group chat based on native sockets using Python.",
             categories: ['native']
-        }, {
+        },
+        {
             image: 'assets/images/old-lcd.jpg',
             link: 'https://www.nagekar.com/2018/05/reusing-old-laptop-lcd-panel.html',
             title: 'Reusing Old LCD Panel',
@@ -101,7 +107,8 @@ let render_projects = (slug) => {
             technologies: ['DIY'],
             description: "Reusing a dead laptop's LCD panel as a secondary monitor.",
             categories: ['diy']
-        }, {
+        },
+        {
             image: 'assets/images/nextcloud-enc.png',
             link: 'https://www.nagekar.com/2017/08/private-cloud-part-2.html',
             title: 'Encrypted Self-Hosted Cloud',
@@ -109,7 +116,8 @@ let render_projects = (slug) => {
             technologies: ['NextCloud', 'GnuPG'],
             description: "Self hosted encrypted cloud setup with Nextcloud and GnuPG.",
             categories: ['diy', 'security']
-        }, {
+        },
+        {
             image: 'assets/images/google-cloud-backup.png',
             link: 'https://www.nagekar.com/2018/05/encrypted-backup-with-duplicity.html',
             title: 'Encrypted Backups - Google Cloud',
@@ -117,7 +125,8 @@ let render_projects = (slug) => {
             technologies: ['NextCloud', 'Duplicity'],
             description: "Create automated encrypted incremental backups of data. Sync everything securely to Google Cloud.",
             categories: ['diy', 'security']
-        }, {
+        },
+        {
             image: 'assets/images/pi-cloud.jpg',
             link: 'https://www.nagekar.com/2016/01/how-to-private-local-cloud-using-raspberrypi.html',
             title: 'Local Cloud - Raspberry Pi',
@@ -125,7 +134,8 @@ let render_projects = (slug) => {
             technologies: ['FTP', 'DIY'],
             description: "Host a local cloud server with a Raspberry Pi and a spare hard disk. Access data instantaneously on any device on the network.",
             categories: ['diy']
-        }, {
+        },
+        {
             image: 'assets/images/LSSSDC-CERTIFICATE-LEVEL-5.jpeg',
             link: 'https://www.dropbox.com/s/xna8sw9t8u9z3rf/LSSSDC-CERTIFICATE-LEVEL-5.jpeg?dl=0',
             title: 'LSSSDC ',
